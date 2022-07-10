1827569d696e46619bd8d1f2ef8a34092a3b86d7
bo98117 <bosin98117@gmail.com>
2022-07-10 13:44:59 -0700

    Print integers from -52 to 1066

---
+++ dont-worry-be-happy.js
@@ -0,0 +1,10 @@
+// Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
+
+
+function beCheerful() {
+    console.log("Good Morning!");
+}
+
+for(let i = 0; i < 98; i++) {
+    beCheerful();
+}
