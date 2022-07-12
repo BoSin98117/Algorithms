d40b002937bf69b0e78ff208071a679ad8bc76ad
bo98117 <bosin98117@gmail.com>
2022-07-11 19:55:14 -0700

    Leap Year

---
+++ leap-year.js
@@ -0,0 +1,14 @@
+// Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, unless it is divisible by 100. However, if it is divisible by 400, then it is.
+
+
+
+function isLeapYear(num) {
+    if(num % 400 == 0 && num % 4 == 0) {
+        console.log("It is a Leap Year");
+    }
+    else if(num % 100 == 0) {
+        console.log("It is NOT a Leap Year!")
+    }
+}
+
+isLeapYear(2100);
