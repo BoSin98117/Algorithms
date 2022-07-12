4b8dbbdd4d5bec065b62d0586e103e1c150a07d8
bo98117 <bosin98117@gmail.com>
2022-07-12 09:15:12 -0700

    What do you know

---
+++ what-do-you-know.js
@@ -0,0 +1,10 @@
+// Your function will be given an input parameter incoming. Please console.log this value.
+
+
+function printParameter(incoming) {
+    console.log(incoming);
+}
+
+printParameter("Hello");
+
+
